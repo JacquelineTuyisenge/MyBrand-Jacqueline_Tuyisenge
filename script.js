d86024a68a12f9b018194693f41e939482070e2d
Jacqueline Tuyisenge <j.tuyisenge@alustudent.com>
2024-02-23 00:42:10 +0200

    contact form Validation

--- script.js
+++ script.js
@@ -9,3 +9,30 @@ window.onscroll = function() {
         header.classList.remove('sticky');
     }
 };
+
+// contact form validation
+
+const name = document.getElementById('name');
+const email = document.getElementById('email');
+const message = document.getElementById('message');
+const form = document.getElementById('contact-form');
+const errorElement = document.getElementById('error');
+
+form.addEventListener('submit', (e) => {
+    //errors
+    let messages = [];
+
+    if (name.value === '' || name.value == null){
+        messages.push('Name is required')
+    }
+
+    if (message.value.length <= 19){
+        messages.push('Message can not be less than 20 characters')
+    }
+
+    if (messages.length > 0){
+        e.preventDefault();
+        errorElement.innerText = messages.join(', ')
+    }
+    
+})
