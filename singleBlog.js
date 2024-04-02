910399a28f7446d4fa6d9226c2a9ea877ee7a8cc
Jacqueline Tuyisenge <j.tuyisenge@alustudent.com>
2024-04-02 03:22:28 +0200

    del blog

--- singleBlog.js
+++ singleBlog.js
@@ -17,9 +17,9 @@ async function fetchBlog(id) {
       popup.innerText = "Error fetching blog";
 
       setTimeout(() => {
-              popup.classList.add("hidden");
-    }, 5000);
-    throw new Error("Error fetching blogs");
+        popup.classList.add("hidden");
+      }, 5000);
+      throw new Error("Error fetching blogs");
     }
     const blog = await response.json();
     loader.style.display = "none";
@@ -68,7 +68,9 @@ function createBlogCard(blog) {
                 </div>
                 <div class="like-nbr">
                     <p id="like-count">${blog.blogLikes.length}</p>
-                    <i class='bx bxs-heart ${blog.isLiked ? 'liked' : ''}' onclick="toggleLike('${blog._id}', this)"></i>
+                    <i class='bx bxs-heart ${
+                      blog.isLiked ? "liked" : ""
+                    }' onclick="toggleLike('${blog._id}', this)"></i>
                 </div>
             </div>
             <div class="add-comment">
@@ -76,7 +78,9 @@ function createBlogCard(blog) {
                     <textarea type="text" placeholder="Add a comment" rows="1" cols="40"></textarea>
                 </div>
                 <div class="add-comment-btn">
-                    <button onclick="addComment('${blog._id}')">Add comment</button>
+                    <button onclick="addComment('${
+                      blog._id
+                    }')">Add comment</button>
                 </div>
             </div>
             ${
@@ -104,7 +108,7 @@ function createBlogCard(blog) {
             </div>
         </section>
     `;
-console.log(`the id of the blog is ${blog._id}`);
+  console.log(`the id of the blog is ${blog._id}`);
   return blogCard;
 }
 
@@ -152,56 +156,59 @@ async function populateBlog(id) {
 // function to add a new comment
 const token = localStorage.getItem("token");
 async function addComment(blogId) {
-    loader.style.display = "block";
-    try{
-        const commentContent = document.querySelector(".comment-place textarea").value.trim();
+  loader.style.display = "block";
+  try {
+    const commentContent = document
+      .querySelector(".comment-place textarea")
+      .value.trim();
 
-        if (!commentContent) {
-            popup.classList.remove("hidden");
-            popup.innerText = "Comment cannot be empty";
-            return;
-        }
-        
-        const response = await fetch(
-            `https://mybrand-be-ecx9.onrender.com/api/blogs/${blogId}/comments`,{
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                    "Authorization": `Bearer ${token}`,
-                },
-                body: JSON.stringify({
-                    comment: commentContent,
-                }),
-            }
-        );
+    if (!commentContent) {
+      popup.classList.remove("hidden");
+      popup.innerText = "Comment cannot be empty";
+      return;
+    }
 
-        if (!response.ok) {
-            loader.style.display = "none";
-                popup.classList.remove("hidden");
-                popup.innerText = 'please sign in to add a comment';
-    
-                setTimeout(() => {
-                    popup.classList.add("hidden");
-                    window.location.href = 'signin.html';
-                }, 3000);  
-        }
+    const response = await fetch(
+      `https://mybrand-be-ecx9.onrender.com/api/blogs/${blogId}/comments`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({
+          comment: commentContent,
+        }),
+      }
+    );
 
-        const newCommentData = await response.json();
-        loader.style.display = "none";
-         popup.classList.remove("hidden");
-        popup.innerText = newCommentData.message;
-        console.log(newCommentData.message);
+    if (!response.ok) {
+      loader.style.display = "none";
+      popup.classList.remove("hidden");
+      popup.innerText = "please sign in to add a comment";
 
-        setTimeout(() => {
-                popup.classList.add("hidden");
-            }, 5000);
-            window.location.reload();
-            console.log(newCommentData);
+      setTimeout(() => {
+        popup.classList.add("hidden");
+      }, 3000);
+      window.location.href = "signin.html";
+    }
 
-        const CommentContainer = document.querySelector(".comments");
-        const commentEl = document.createElement("div");
-        commentEl.classList.add("comment");
-        commentEl.innerHTML = `
+    const newCommentData = await response.json();
+    loader.style.display = "none";
+    popup.classList.remove("hidden");
+    popup.innerText = newCommentData.message;
+    console.log(newCommentData.message);
+
+    setTimeout(() => {
+      popup.classList.add("hidden");
+    }, 5000);
+    window.location.reload();
+    console.log(newCommentData);
+
+    const CommentContainer = document.querySelector(".comments");
+    const commentEl = document.createElement("div");
+    commentEl.classList.add("comment");
+    commentEl.innerHTML = `
             <div class="profile-comment">
                 <div>
                     <p id="comment-author">${newCommentData.author}</p>
@@ -211,84 +218,82 @@ async function addComment(blogId) {
             </div>
             <hr>
         `;
-        CommentContainer.appendChild(commentEl);
+    CommentContainer.appendChild(commentEl);
 
-        document.querySelector(".comment-place textarea").value = "";
+    document.querySelector(".comment-place textarea").value = "";
 
-        //update comment count
+    //update comment count
 
-        const commentCountElement = document.querySelector(".comment-nbr p");
-        const currentCommentCount = parseInt(commentCountElement.textContent);
-        commentCountElement.textContent = currentCommentCount + 1;
-
-    } catch (error) {
-        console.log(error);
-        loader.style.display = "none";
-        popup.classList.remove("hidden");
-        popup.innerText = "Error adding comment";
-        return;
-    }
+    const commentCountElement = document.querySelector(".comment-nbr p");
+    const currentCommentCount = parseInt(commentCountElement.textContent);
+    commentCountElement.textContent = currentCommentCount + 1;
+  } catch (error) {
+    console.log(error);
+    loader.style.display = "none";
+    popup.classList.remove("hidden");
+    popup.innerText = "Error adding comment";
+    return;
+  }
 }
 
 //funtion to toggle like
 
 async function toggleLike(blogId, icon) {
-    try {
-        const token = localStorage.getItem("token");
-        console.log("Token:", token);
+  try {
+    const token = localStorage.getItem("token");
+    console.log("Token:", token);
 
-        const response = await fetch(
-            `https://mybrand-be-ecx9.onrender.com/api/blogs/${blogId}/likes`, {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                    "Authorization": `Bearer ${token}`,
-                },
-            }
-        );
+    const response = await fetch(
+      `https://mybrand-be-ecx9.onrender.com/api/blogs/${blogId}/likes`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
 
-        if (!response.ok) {
-            console.error("Error toggling like:", response.status);
-            popup.classList.remove("hidden");
-            popup.innerText = "Error toggling like";
-            setTimeout(() => {
-                popup.classList.add("hidden");
-            }, 2000);
-        }
-
-        const data = await response.json();
-        console.log("Response data:", data);
-        popup.classList.remove("hidden");
-        popup.innerText = data.message;
-        setTimeout(() => {
-            popup.classList.add("hidden");
-        }, 5000);
-
-        const likeCountElement = document.getElementById("like-count");
-        if (!likeCountElement) {
-            console.error("Like count element not found.");
-            return;
-        }
-
-       const likedKey = `liked_${blogId}`;
-        if (data.message === "Blog liked successfully") {
-            icon.classList.add("liked");
-            likeCountElement.textContent = parseInt(likeCountElement.textContent) + 1;
-            // Store liked status locally
-            localStorage.setItem(likedKey, true);
-            console.log("Liked status stored:", localStorage.getItem(likedKey));
-        } else if (data.message === "Blog unliked successfully") {
-            icon.classList.remove("liked");
-            likeCountElement.textContent = parseInt(likeCountElement.textContent) - 1;
-            localStorage.removeItem(likedKey);
-            console.log("Liked status removed:", localStorage.getItem(likedKey));
-        }
-    } catch (error) {
-        console.error("Error liking/unliking blog", error.message);
+    if (!response.ok) {
+      console.error("Error toggling like:", response.status);
+      popup.classList.remove("hidden");
+      popup.innerText = "Error toggling like";
+      setTimeout(() => {
+        popup.classList.add("hidden");
+      }, 2000);
     }
-};
 
+    const data = await response.json();
+    console.log("Response data:", data);
+    popup.classList.remove("hidden");
+    popup.innerText = data.message;
+    setTimeout(() => {
+      popup.classList.add("hidden");
+    }, 5000);
 
+    const likeCountElement = document.getElementById("like-count");
+    if (!likeCountElement) {
+      console.error("Like count element not found.");
+      return;
+    }
+
+    const likedKey = `liked_${blogId}`;
+    if (data.message === "Blog liked successfully") {
+      icon.classList.add("liked");
+      likeCountElement.textContent = parseInt(likeCountElement.textContent) + 1;
+      // Store liked status locally
+      localStorage.setItem(likedKey, true);
+      console.log("Liked status stored:", localStorage.getItem(likedKey));
+    } else if (data.message === "Blog unliked successfully") {
+      icon.classList.remove("liked");
+      likeCountElement.textContent = parseInt(likeCountElement.textContent) - 1;
+      localStorage.removeItem(likedKey);
+      console.log("Liked status removed:", localStorage.getItem(likedKey));
+    }
+  } catch (error) {
+    console.error("Error liking/unliking blog", error.message);
+  }
+}
 
 // Event listener for DOMContentLoaded event
 document.addEventListener("DOMContentLoaded", function () {
